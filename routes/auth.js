31e32e67aa916a06fb134a01d9e27f254f745fb1
war21001 <83742731+war21001@users.noreply.github.com>
2022-02-05 14:01:36 -0700

    pattern for password
    added a pattern attribute to signup.html. In the password, a user must have a number, an upper and lower case letter and it must be at least 8 characters long

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth');
+
 router.post('/sign-up', authController.signUp);
 router.post('/login', authController.login);
 
