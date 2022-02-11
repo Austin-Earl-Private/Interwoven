88c32ed5b4edbbcd568b1cda6cabf67c58048d3e
Tanner Roberts <tannerbroberts@gmail.com>
2022-02-10 19:20:39 -0700

    database search with query parameters works.

--- controllers/search.js
+++ controllers/search.js
@@ -4,8 +4,10 @@ const Story = require('../models/Story');
 exports.search = (req, res, next) => {
     // todo: This isn't recieving query parameters from the url ?key=value
     // let country = req.body.country;
+    let country = req.query.country;
+    console.log(country);
 
-    Story.find().then((stories) => {
+    Story.find({country_code: country}).then((stories) => {
         res.status(201).json({
             searchStories: stories,
         });
