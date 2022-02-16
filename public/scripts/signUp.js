b243a21856779da2749281bd568ac67efa2278e4
war21001 <83742731+war21001@users.noreply.github.com>
2022-02-15 21:43:22 -0700

    signup.js
    start of signup.js
    getting an error that /auth/signUp not found

---
+++ public/scripts/signUp.js
@@ -0,0 +1,30 @@
+function signUp() {
+    // e.preventDefault();
+    // const form = document.querySelector('#login');
+    // const inputs = form.elements;
+    // const email = inputs['email'];
+    // const password = inputs['password'];
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+
+    const localStorage = window.localStorage;
+
+    fetch('http://localhost:8080/auth/signUp', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json, text/plain, */*',
+        },
+        body: JSON.stringify({firstName: firstName, lastName: lastName, email: email, password: password }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('isMod', data.isMod);
+            window.location.href = 'http://localhost:8080/views/profile.html';
+        });
+    return false;
+}
