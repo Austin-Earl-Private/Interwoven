a2d4b76bad086d08f9a464a131b77430b4475f32
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-02-18 15:09:46 -0700

    add story frontend

--- controllers/story.js
+++ controllers/story.js
@@ -30,3 +30,32 @@ exports.addStory = (req, res, next) => {
             next(err);
         });
 };
+
+exports.deleteStory = (req, res, next) => {
+    const storyId = req.body.storyId;
+    const userId = req.userId;
+    const isMod = req.userId;
+    User.findById(userId)
+        .then((user) => {
+            if (!user) {
+                throw new Error('Could not find user, Try again.');
+            }
+            return Story.findById(storyId);
+        })
+        .then((story) => {
+            if (isMod || story.creator == userId) {
+                return story.delete();
+            } else {
+                throw new Error(
+                    'You dont have persmissions to delete this story.'
+                );
+            }
+        })
+        .then((result) => {
+            return res.status(200).json({ id: story._id });
+        })
+        .catch((err) => {
+            err.statusCode = 500;
+            next(err);
+        });
+};
